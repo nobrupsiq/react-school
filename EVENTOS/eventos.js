fd44cf5a7dbcf30a296781926a978bd346909843
bruno <bpires.siqueira@gmail.com>
2022-11-30 21:45:03 -0300

    react eventos

---
+++ EVENTOS/eventos.js
@@ -0,0 +1,2 @@
+// Eventos
+//Podemos atribuir eventos diretamente aos elementos JSX como um atributo. Os eventos são sintáticos, ou seja, são criados pelo própio React porém seguindo as especificações da W3C (e funcionam igualmente nos diversos browsers que o React suporta).
--- react-introducao/src/App.js
+++ react-introducao/src/App.js
@@ -1,43 +1,17 @@
 import React from "react";
 
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ['#29d8d5', '#252a34', '#fc3766']
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ['#ffd045', '#d4394b', '#f37c59']
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ['#365069', '#47c1c8', '#f95786']
-  }
-];
-
 const App = () => {
-  const dados = produtos.filter(({ preco }) => Number(preco.replace("R$", "")) > 1500)
-  console.log(dados)
+  function handleScroll(event) {
+    console.log(event);
+  }
 
-  return < section >
-    {dados.map(({ id, nome, preco, cores }) => (
-      <div key={id}>
-        <h1>{nome}</h1>
-        <p>Preço: {preco}</p>
-        <ul>
-          {cores.map((cor) => (
-            <li style={{ backgroundColor: cor, color: "white" }} key={cor}>{cor}</li>
-          ))}
-        </ul>
-      </div>
-    ))}
-  </ section>
+  window.addEventListener('scroll', handleScroll)
+
+  return (
+    <div style={{ height: '200vw' }}>
+      Div
+    </div>
+  )
 };
 
 export default App;
