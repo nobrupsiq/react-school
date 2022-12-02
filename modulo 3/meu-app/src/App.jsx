fc581202d8520cace33209b3e4b0ec186cd9880e
bruno <bpires.siqueira@gmail.com>
2022-12-02 10:55:43 -0300

    MD3: EX projeto em grupo

---
+++ modulo 3/meu-app/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from "axios";
+
+function App() {
+  const [posts, setPosts] = useState([])
+  const apiEndPoint = 'https://jsonplaceholder.typicode.com/posts'
+  useEffect(() => {
+    const getPosts = async () => {
+      const {data: res} = await axios.get(apiEndPoint)
+      setPosts(res)
+    }
+    getPosts()
+  }, [])
+
+  return (
+    <div className="container">
+      <h2>Quantidade de itens:  {posts.length} Na minha API</h2>
+      <button className='btn btn-info btn-sm'>Inserir</button>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Atualização</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          { posts.map((posts) => (
+            <tr key={posts.id}>
+              <td>{posts.title}</td>
+              <td>
+                <button className='btn btn-info btn-sm'>Update</button>
+              </td>
+              <td>
+                <button className='btn btn-danger btn-sm'>Delete</button>
+              </td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
