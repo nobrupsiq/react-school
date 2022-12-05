139ac4040c746373285d090bdf39952d953cd5bf
bruno <bpires.siqueira@gmail.com>
2022-12-05 08:01:27 -0300

    update

--- meu-app/src/App.jsx
+++ meu-app/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const apiEndPoint = 'https://jsonplaceholder.typicode.com/posts'
+  const [posts, setPosts] = useState([]);
+  const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
   useEffect(() => {
     const getPosts = async () => {
-      const {data: res} = await axios.get(apiEndPoint)
-      setPosts(res)
-    }
-    getPosts()
-  }, [])
+      const { data: res } = await axios.get(apiEndPoint);
+      setPosts(res);
+    };
+    getPosts();
+  }, []);
 
   return (
     <div className="container">
-      <h2>Quantidade de itens:  {posts.length} Na minha API</h2>
-      <button className='btn btn-info btn-sm'>Inserir</button>
-      <table className='table'>
+      <h2>Quantidade de itens: {posts.length} Na minha API</h2>
+      <button className="btn btn-info btn-sm">Inserir</button>
+      <table className="table">
         <thead>
           <tr>
             <th>Titulo</th>
@@ -27,21 +27,21 @@ function App() {
         </thead>
 
         <tbody>
-          { posts.map((posts) => (
+          {posts.map((posts) => (
             <tr key={posts.id}>
               <td>{posts.title}</td>
               <td>
-                <button className='btn btn-info btn-sm'>Update</button>
+                <button className="btn btn-info btn-sm">Update</button>
               </td>
               <td>
-                <button className='btn btn-danger btn-sm'>Delete</button>
+                <button className="btn btn-danger btn-sm">Delete</button>
               </td>
             </tr>
-          )) }
+          ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
